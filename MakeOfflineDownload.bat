66e0a25462ec0b2ffecb0f19ec879eb614ce3434
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-04-08 14:12:12 +0330

    Revert "upload workspace"
    This reverts commit 3a8adbe33e98af8ec548220fb1b651da4853a737.

--- MakeOfflineDownload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title MakeOfflineDownload
-java -cp "desktopRuntime/MakeOfflineDownload.jar;desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/EaglerForge_v1.3_1.8_Offline_en_US.html" "javascript/EaglerForge_v1.3_1.8_Offline_International.html" "javascript/lang"
-pause
